07f631e94c0744db6ab50bc877042378e11798b0
shamsWMM <smohammedmawloo@deakin.edu.au>
2024-01-01 01:20:36 +1100

    fixed issues with sort and insert functions

--- sort.c
+++ sort.c
@@ -3,10 +3,8 @@
 #include <time.h>
 #define LEN 10
 void bsort(int *nums, int len);
-int *sort(int *nums, int len); // does not modify original array
-void insert(int i, int *nums, int len, int *res); 
-int *sortv(int *nums, int len); // modifies original array
-void insertv(int i, int *nums, int len, int *ptr);
+int *sort(int *nums, int len); // modifies original array
+void insert(int i, int *nums, int len, int *ptr);
 void display(int *nums, int len);
 
 int main()
@@ -24,79 +22,32 @@ int main()
 
 	tic = clock();
 //    bsort(numbers,LEN);
-//    nums = sort(numbers,LEN);
-    sortv(numbers,LEN);
+    sort(numbers,LEN);
     toc = clock();
     display(numbers,LEN);
-//    display(nums,LEN);
 	printf("Elapsed for function: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
     
 	return(0);
 }
 
-// trying without malloc() - this modifies original int array values
-int *sortv(int *nums, int len)
-// assume len is at least 1
-{
-    if(len==1) return(nums);
-    insertv(*nums, sort(nums+1, len-1), len-1, nums);
-    return(nums);
-}
-void insertv(int i, int *nums, int len, int *ptr)
-{
-    if(len==0)
-    {
-        *ptr = i;
-    }
-    else
-    {
-        if(i<*nums)
-        {
-            *ptr = i;
-            insertv(*nums,nums+1,len-1,ptr+1); //?
-        }
-        else
-        {
-            *ptr = *nums;
-            insertv(i,nums+1,len-1,ptr+1);
-        }
-    }
-}
-
-
-// trying with malloc()
 int *sort(int *nums, int len)
 // assume len is at least 1
 {
     if(len==1) return(nums);
-    //allocate memory for the sorted result
-    int *res = (int *)malloc(len*sizeof(int));
-    //if (result == NULL) {
-    //    // Handle memory allocation failure
-    //    fprintf(stderr, "Memory allocation failed.\n");
-    //    exit(EXIT_FAILURE);
-    //}
-    insert(*nums, sort(nums+1, len-1), len-1, res);
-    return(res);
+    insert(*nums, sort(nums+1, len-1), len-1, nums);
+    return nums;
 }
-void insert(int i, int *nums, int len, int *res)
-{
-    if(len==0)
-    {
-        *res = i;
-    }
-    else 
+void insert(int i, int *nums, int len, int *ptr)
+{ // assumes list is already sorted
+    if(len==0)*ptr = i;
+    else
     {
         if(i>*nums)
         {
-            *res = *nums;
-            insert(i,nums+1,len-1,res+1);
+            *ptr=*nums;
+            insert(i,nums+1,len-1,ptr+1);
         }
-        else
-        {
-            *res = i;
-            insert(*nums,nums+1,len-1,res+1);
-        }
+        else *ptr = i;
     }
 }
 
