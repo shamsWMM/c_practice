9c033773908cec7eb472ab32ca7f647101fb461e
shamsWMM <smohammedmawloo@deakin.edu.au>
2024-01-01 03:02:29 +1100

    added render_sorted and r_insert functions

--- sort.c
+++ sort.c
@@ -4,29 +4,30 @@
 #define LEN 10
 void bsort(int *nums, int len);
 int *sort(int *nums, int len); // modifies original array
+int *render_sorted(int *nums, int len); // does not modify original array
 void insert(int i, int *nums, int len, int *ptr);
+void insert_r(int i, int *nums, int len, int *ptr);
 void display(int *nums, int len);
 
 int main()
 {
-    clock_t tic, toc;
-    int *nums;
-
+    // generate array to be sorted
     int numbers[10];
-    // populate the array with integers from 1 to 100
-	srand((unsigned)time(NULL));
+	srand((unsigned)time(NULL)); 
     int i;
-	for(i=0;i<LEN;i++) numbers[i] = rand() % 100 + 1;
+	for(i=0;i<LEN;i++) numbers[i] = rand() % 100 + 1; 
     //display array
     display(numbers, LEN);
-
-	tic = clock();
-//    bsort(numbers,LEN);
-    sort(numbers,LEN);
-    toc = clock();
-    display(numbers,LEN);
-	printf("Elapsed for function: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
     
+    int *nums; // for the result of render_sorted()
+    nums = render_sorted(numbers,LEN);
+    display(nums,LEN);
+
+    display(numbers, LEN); //dsiplay original array to show it has not changed
+
+//    bsort(numbers,LEN);
+    sort(numbers,LEN); //sort original array
+    display(numbers,LEN); //display original array  
 	return(0);
 }
 
@@ -45,9 +46,34 @@ void insert(int i, int *nums, int len, int *ptr)
         if(i>*nums)
         {
             *ptr=*nums;
-            insert(i,nums+1,len-1,ptr+1);
+            insert_r(i,nums+1,len-1,ptr+1);
         }
         else *ptr = i;
+    }
+}
+//with malloc
+int *render_sorted(int *nums, int len)
+{
+    int *res = (int *)malloc(len*sizeof(int));
+    if(len==1) *res=*nums;
+    else insert_r(*nums, render_sorted(nums+1, len-1), len-1, res);
+    return res;
+}
+void insert_r(int i, int *nums, int len, int *res)
+{ // assumes list is already sorted
+    if(len==0)*res = i;
+    else
+    {
+        if(i>*nums)
+        {
+            *res=*nums;
+            insert_r(i,nums+1,len-1,res+1);
+        }
+        else 
+        {
+            *res = i;
+            insert_r(*nums,nums+1,len-1,res+1);
+        }
     }
 }
 
