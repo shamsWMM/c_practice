5018467d16e79673e4705d9f80fb8dec7bcc5cc0
shamsWMM <101496933+shamsWMM@users.noreply.github.com>
2024-01-09 21:06:36 +1100

    Update sort.c

--- sort.c
+++ sort.c
@@ -5,6 +5,7 @@
 
 void sort(int *nums, int len);
 void insert(int i, int *nums, int len);
+void generate(int *array, int len); // generate array of length l
 void display(int *nums, int len);
 int main()
 {
@@ -13,18 +14,12 @@ int main()
 
     for (variable_len = 1; variable_len < 7; variable_len++)
     {
-        // generate array to be sorted
         int numbers[variable_len];
-        int i;
-        for (i = 0; i < variable_len; i++)
-        {
-            int j = rand() % 100 + 1;
-            numbers[i] = j;
-        }
-        // display array
-        display(numbers, variable_len);
-        sort(numbers, variable_len); // sort original array
-        display(numbers, variable_len);
+        generate(numbers, variable_len); // generate array to be sorted
+        display(numbers, variable_len);  // display array
+
+        sort(numbers, variable_len);    // sort original array
+        display(numbers, variable_len); // display sorted array
     }
     return (0);
 }
@@ -34,7 +29,7 @@ void sort(int *nums, int len)
 {
     if (len)
     {
-        sort(nums+1, len-1);
+        sort(nums + 1, len - 1);
         insert(*nums, nums + 1, len - 1);
     }
 }
@@ -44,36 +39,42 @@ void insert(int i, int *nums, int len)
     if (len == 0)
         *(nums - 1) = i;
     else if (i > *nums) // number to be inserted is larger than first number
-        {
-            *(nums - 1) = *nums;
-            insert(i, nums + 1, len - 1); // use insert on the number and the rest of the array
-        }
-    else *(nums - 1) = i; // number to be inserted is smaller than first number in the array
+    {
+        *(nums - 1) = *nums;
+        insert(i, nums + 1, len - 1); // use insert on the number and the rest of the array
+    }
+    else
+        *(nums - 1) = i; // number to be inserted is smaller than first number in the array
 }
-
 
 // the bubble sort function for comparison
 void bsort(int *nums, int len)
 {
-    int i, j, temp;
-    for (i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
     {
-        for (j = i + 1; j < len; j++)
+        for (int j = i + 1; j < len; j++)
         {
             if (*(nums + i) > *(nums + j))
             {
-                temp = *(nums + i);
+                int temp = *(nums + i);
                 *(nums + i) = *(nums + j);
                 *(nums + j) = temp;
             }
         }
     }
 }
+void generate(int *array, int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        int j = rand() % 100 + 1;
+        *(array + i) = j;
+    }
+}
 void display(int *nums, int len)
 {
-    int i;
     puts("Displaying the array:");
-    for (i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
         printf(" %3d", *(nums + i));
     putchar('\n');
 }
