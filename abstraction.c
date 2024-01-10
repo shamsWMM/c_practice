6fce00e2ba6159e41438dc340e22f77615d8187f
shamsWMM <smohammedmawloo@deakin.edu.au>
2024-01-10 12:13:09 +1100

    fixed function signatures and added filter_ints

--- abstraction.c
+++ abstraction.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void map_ints(void fun(int *ptr), int *arr, int len);
+void map_ints(void (*fun)(int *ptr), int *arr, int len);
+void filter_ints(int (*fun)(int i), int *arr, int len);
 void square(int *i);
+int is_even(int i);
 void display(int *nums, int len);
 
 int main(int argc, char **argv)
 {
-    int ints[] = {1, 2, 3, 4};
+    int len = 4, ints[] = {1, 2, 3, 4};
     display(ints, 4);
-    int len = 4;
+
     map_ints(square, ints, len);
+    display(ints, 4);
+
+    filter_ints(is_even, ints, len);
     display(ints, 4);
 
     return 0;
 }
 // map
-void map_ints(void fun(int *ptr), int *arr, int len)
+void map_ints(void (*fun)(int *ptr), int *arr, int len)
 {
     while (len)
     {
@@ -25,10 +30,24 @@ void map_ints(void fun(int *ptr), int *arr, int len)
         len--;
     }
 }
+// filter
+void filter_ints(int (*fun)(int i), int *arr, int len)
+{
+    if (len)
+    {
+        if (!fun(*arr))
+            *arr = 0;
+        filter_ints(fun, ++arr, --len);
+    }
+}
 
 void square(int *i)
 {
     *i *= *i;
+}
+int is_even(int i)
+{
+    return !(i % 2);
 }
 void display(int *nums, int len)
 {
